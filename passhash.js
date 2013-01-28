bd0a7f65a8f178cb328efbb360dac47a31690b37
Alex Shagla-McKotch <alex.shaglamckotch@voxer.com>
2013-01-27 23:31:42 -0800

    added usage statement. added command line arguments. added custom output forwarding.

--- passhash.js
+++ passhash.js
@@ -1,5 +1,5 @@
 /*
- * password hashing using sha512 and 128 bit crypto random salt
+ * password hashing using sha512 and random salt
  *
  * Author: Alexander Shagla-McKotch
  *
@@ -8,7 +8,35 @@
 
 // initial variables and requires
 var prom = require('prompt'),
-    crypto = require('crypto');
+    crypto = require('crypto'),
+    getopt = require('posix-getopt')
+    package = require('./package.json');
+
+var format = '{name}:{salt}:{hash}:{iterations}'
+
+/*
+ * Usage statement
+ *
+ * return the usage statement
+ */
+
+function usage() {
+  return [
+    'Usage: passhash',
+    '',
+    'Easily and securily hash passwords with a variable amount of iterations of SHA512.',
+    '',
+    'Takes in an optional username and number of iterations and then propmts for a password.',
+    '',
+    'If a username or number of iterations is not provided it will prompt for them.',
+    '',
+    '-i, --iterations <number>        number of SHA512 iterations (default is set to 1)',
+    '-h, --help                       print this message and exit',
+    '-u, --username <name>            username to use for entry',
+    '-U, --updates                    check for available updates',
+    '-v, --version                    print the version number and exit'
+  ].join('\n');
+}
 
 /* schema for prompt
  * checks if the username is only letters and numbers
@@ -33,13 +61,42 @@ var schema = {
        required: true
      },
      iterations: {
-      description: 'Number of iteration (default 1)'.magenta,
-      pattern: /[0-9]+$/,
+      description: 'Number of iterations (default 1)'.magenta,
+      pattern: /^[0-9]+$/,
       message: 'Must be a number',
       default: 1,
     }
   }
 };
+
+// get command line arguments
+var options = [
+  'f:(format)',
+  'i:(iterations)',
+  'h(help)',
+  'u:(username)',
+  'U(updates)',
+  'v(version)'
+].join('');
+var parser = new getopt.BasicParser(options, process.argv);
+var iterations = 1;
+var username;
+while ((option = parser.getopt()) !== undefined) {
+  switch (option.option) {
+    case 'f': format = option.optarg; break;
+    case 'i': iterations = option.optarg; delete schema.properties.iterations; break;
+    case 'h': console.log(usage()); process.exit(0);
+    case 'u': username = option.optarg; delete schema.properties.user_name; break;
+    case 'U':require('latest').checkupdate(package, function(ret, msg) {
+        console.log(msg);
+        process.exit(ret);
+      });
+      return;
+    case 'v': console.log(package.version); process.exit(0);
+    default: console.error(usage()); process.exit(1); break;
+  }
+}
+var args = process.argv.slice(parser.optind());
 
 prom.message = 'passhash'.cyan;
 // start the prompt
@@ -47,6 +104,12 @@ prom.start();
 
 // prompt user for input
 prom.get(schema, function (err, result) {
+
+  // if username was not provided on command line set username from prompt
+  if (result.username) username = result.username;
+
+  // if iterations is not provided on command line set iterations from prompt
+  if (result.iterations) iterations = result.iterations;
 
   // check for password mismatch and exits on mismatch
   if (result.password !== result.repeat_password) {
@@ -63,9 +126,14 @@ prom.get(schema, function (err, result) {
     if (err) throw err;
     var salt = buf.toString('base64');
     var hash = result.password;
-    for (var i = 0; i<=result.iterations; i++) {
+    for (var i = 0; i<=iterations; i++) {
       hash = crypto.createHmac('sha512', salt).update(hash).digest('hex');
     }
-    console.log('%s:%s:%s:%s', result.user_name.green, salt.red, hash.blue, result.iterations.magenta);
+    var s = format
+              .replace('{username}', username)
+              .replace('{salt}', salt)
+              .replace('{hash}', hash)
+              .replace('{iterations}', iterations);
+    console.log(s);
   });
 });
