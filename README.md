ff060196322b1793aae0d2978459bba07880ef07
Alex Shagla-McKotch <alex.shaglamckotch@voxer.com>
2013-04-02 16:25:50 -0700

    added support for bcrypt. changed prompts, command line options and updated README.

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ passhash
 
 Easily and securely hash passwords with a variable amount of iterations of SHA512.
 
+You can also specify to use bcrypt and the number of rounds to use.
+
 Takes in an optional username and number of iterations and then prompts for a password.
 
-If a username or number of iterations is not provided it will prompt for them.
+If a username or number of iterations/rounds is not provided it will prompt for them.
 
 Installation
 ------------
@@ -15,20 +17,30 @@ Installation
 Usage
 -----
 
+    -t, --hash-type <number>         1 for SHA512 or 2 for bcrypt
     -i, --iterations <number>        number of SHA512 iterations (default is set to 5000)
+    -r, --rounds <number>            number of rounds for bcrypt
     -b, --bytes <number>             number of bytes to use for crypto random salt, must be >= 128 (default 128)
     -h, --help                       print this message and exit
     -u, --username <name>            username to use for entry
     -U, --updates                    check for available updates
-    -f, --format                     change format of output
+    -f, --format                     change format of output. Not supported if using bcrypt as hash type
     -v, --version                    print the version number and exit
 
 Default Output
 --------------
 
+###SHA512
+
     username:salt:hash:iterations
 
     test:0V6lfASr44DcJexA0FawChE0ZGo+xM9zBjJMC3S5vp0RH0SdhpS/LIYPI7tP/gms5JVZ2fcnShClgmQif84rdxFQ3A88vZFzIpSBGMSzMabyqPlvzm2BwpS7WLqMbJlXdtSNriQ+PfkKmFj2+oeOS9BNHl2MgSUk8iXu5LB7LXGSinO4k2AsJqB+eslJSL1uzCpA5Ww2weHt47aU+vKWDiuodyPFlL3ahbYAt3nqlkbtr7E0X15nKiEOIWTtdRENCVFyIUf0PLSRYLLZwdnQ5sl414QN8NFcPtDSXEoAblJlcTCa+VJgXx2T2RKdg1RC360GyGsaHtZN9mY8y4Puermp4LWyg1reM9gkXXae63h3pzYjaQc5emSdK+0ljIMKI4aR/wkkLrbnS4/hbeqaE6L1+/X1QPHR0u9BQWAUsUqED6IiStDlSdidn9jBBOAdTIcXPHm6r0fBjoYx2mWmXCdMubF04ojjhsiCY/rcwYZZpjBRYtH8v5H1FWr1QUpzjKETLZml3c3bNAriX9e1GsX8wQqcfDpu/5pKSorfF5qmoIthr5uLsAavfO0XJ9D8C97G3sw91hVazGnvA6ER+PoUx4FZPJYxOFC3dfkOo8CSzOuTgQQaXmlEHJ6TcsVQ6kBMApUaUqqulaxZkZKtZEUu1zlW5CvbIf/kV5z5GtQ=:2a0293e90184f6a9d569a794bd7cc8b46d38e4c4faae54e2f8221463c99ee4ebe46805f629d8189c8c64227833130d5d9f7caea1d9c0a5c5f7e238a0e9f4a149:1444
+
+###bcrypt
+
+    username:hash
+
+    test:$2a$10$na0tH7pxC4ov9ZPGLgkvFO3YNuElwd7hNSoBRH3k3rHQXsj8TBSja
 
 Change Output Format
 --------------------
@@ -47,22 +59,23 @@ This would output:
 Example Prompt
 --------------
 
-    alex @ [ kovas :: (Darwin) ] ~/Projects/passhash $ node passhash.js 
+    alex @ [ kovas :: (Darwin) ] ~/Projects/passhash $ ./passhash.js
+    passhash: Enter 1 for SHA512 or 2 for bcrypt:  2
     passhash: username:  test
-    passhash: Please enter a password:  
-    passhash: Please re-enter your password:  
-    passhash: Number of iterations (default 5000):  (5000) 
-    test:1N1SnR2+aqJPLgtdBZ3Bip4epRzLNAazg/cdqAOIDx65yxiifsf50DE/vXbHFoXtrM2ZbP3N2hq+d20LwkzwLDks//4E726se8iz8cwSgZjnKIYfzGUD6SPtZr7GQuR60TpC7qL9D9tXVHGjz+XS0E1AqaF7sdVna+2FmzQbgLc=:a9901ced04776219b0c59468aba567d16252ad17463ca8d7933206dde70e11b5e914be33d1611dde03e772ddc1dbfd70d5ef6368d9d7e7112ef77d6a539d1d3a:5000
+    passhash: Please enter a password:
+    passhash: Please re-enter your password:
+    passhash: Number of iterations for SHA512 or number of rounds for bcrypt :  10
+    test:$2a$10$lqem0mDx5ZnV272iTWgnnOu8aG.m5Ts7aCbHuqVdTuAFe.K2MgzLa
 
 Why?
 ----
 
 I personally dislike how people do authentication on the web. `htpasswd` can easily be broken. `passhash` allows for a more robust password storage system.
-Now you can store the output of `passhash` into a file and code your node server to use it for auth, eliminating the need for htaccess/htpasswd.
+Now you can store the output of `passhash` into a file and code your node server to use it for authentication, eliminating the need for htaccess/htpasswd.
 For added security you can choose a number of iterations and not store it in the file, using it only in your server code for the password validation.
 Each user could also have a different number of iterations. Is this still somewhat insecure? Yes, someone could take a line from the file and
 if the user choose a weak password and the attacker knew the number of iterations and the salt they could still use brute force.
-You can't protect against stupid users.
+You can't protect against stupid users. That is why I have added in `bcrypt` support in the latest release. 
 
 License
 -------
--- passhash.js
+++ passhash.js
@@ -4,6 +4,7 @@
  *
  * password hashing using sha512 and random salt
  *
+ * As of 0.0.7 support for bcrypt
  * Author: Alexander Shagla-McKotch <shagla@gmail.com>
  *
  * Contributor: Dave Eddy <dave@daveeddy.com> github.com/bahamas10
@@ -14,7 +15,8 @@
 // initial variables and requires
 var prom = require('prompt'),
     crypto = require('crypto'),
-    getopt = require('posix-getopt')
+    getopt = require('posix-getopt'),
+    bcrypt = require('bcrypt'),
     package = require('./package.json');
 
 var format = '{username}:{salt}:{hash}:{iterations}'
@@ -31,16 +33,20 @@ function usage() {
     '',
     'Easily and securely hash passwords with a variable amount of iterations of SHA512.',
     '',
+    'You can also specify to use bcrypt and the number of rounds to use',
+    '',
     'Takes in an optional username and number of iterations and then prompts for a password.',
     '',
     'If a username or number of iterations is not provided it will prompt for them.',
     '',
+    '-t --hash-type <number>          1 for SHA512 or 2 for bcrypt',
     '-i, --iterations <number>        number of SHA512 iterations (default is set to 5000)',
+    '-r, --rounds <number>            number of rounds for bcrypt',
     '-b, --bytes <number>             number of bytes to use for crypto random salt, must be >= 128 (default 128)',
     '-h, --help                       print this message and exit',
     '-u, --username <name>            username to use for entry',
     '-U, --updates                    check for available updates',
-    '-f, --format                     change format of output',
+    '-f, --format                     change format of output. Not supported if using bcrypt as hash type.',
     '-v, --version                    print the version number and exit',
     '',
     'Deafault output',
@@ -63,6 +69,12 @@ function usage() {
  */
 var schema = {
     properties: {
+      hash_type: {
+        description: 'Enter 1 for SHA512 or 2 for bcrypt'.magenta,
+        pattern: /^1|2$/,
+        message: 'Must be either 1 or 2',
+        required: true
+      },
       user_name: {
         description: 'username'.magenta,
         pattern: /^[a-z0-9\.@_]+$/,
@@ -80,17 +92,18 @@ var schema = {
        required: true
      },
      iterations: {
-      description: 'Number of iterations (default 5000)'.magenta,
+      description: 'Number of iterations for SHA512 or number of rounds for bcrypt '.magenta,
       pattern: /^[0-9]+$/,
-      message: 'Must be a number',
-      default: 5000,
+      message: 'Must be a number'
     }
   }
 };
 
 // get command line arguments
 var options = [
+  't:(hash-type)',
   'f:(format)',
+  'r:(rounds)',
   'b:(bytes)',
   'i:(iterations)',
   'h(help)',
@@ -100,11 +113,21 @@ var options = [
 ].join('');
 var parser = new getopt.BasicParser(options, process.argv);
 var iterations = 1;
+var rounds = 1;
 var username;
+var type;
 var bytes = 128;
 while ((option = parser.getopt()) !== undefined) {
   switch (option.option) {
     case 'f': format = option.optarg; break;
+    case 't': type = option.optarg; if (type != 1 && type != 2) {
+                console.log('ERROR: Hash type must be either 1 or 2.');
+                process.exit(1);
+              } delete schema.properties.hash_type; break;
+    case 'r': rounds = option.optarg; if (+rounds < 0) {
+                console.log('ERROR: Number of rounds must be larger than 0.');
+                process.exit(1);
+              } delete schema.properties.iterations; break;
     case 'b': bytes = option.optarg; if (+bytes < 128) {
                 console.log('ERROR: Number of bytes must be larger than 128.');
                 process.exit(1);
@@ -130,8 +153,15 @@ prom.start();
 // prompt user for input
 prom.get(schema, function (err, result) {
 
+  // if hash type was not provided on command line set type from prompt
+  if (result.hash_type) type = result.hash_type;
+
   // if username was not provided on command line set username from prompt
   if (result.user_name) username = result.user_name;
+
+  // if rounds is not provided on command line set rounds from prompt
+  // the same prompt is used for rounds and iterations
+  if (result.iterations) rounds = result.iterations;
 
   // if iterations is not provided on command line set iterations from prompt
   if (result.iterations) iterations = result.iterations;
@@ -141,24 +171,29 @@ prom.get(schema, function (err, result) {
     console.log('ERROR: Password mismatch.');
     process.exit(1);
   }
-
-  /* generate 128 bit crypto random bytes
-   * save the byte buffer as a base64 encoded salt
-   * using the salt and password create a salted sha512 hash
-   * output the username, salt, and salted hash ':' delimited
-   */
-  crypto.randomBytes(+bytes, function(err, buf) {
-    if (err) throw err;
-    var salt = buf.toString('base64');
-    var hash = result.password;
-    for (var i = 0; i<=iterations; i++) {
-      hash = crypto.createHmac('sha512', salt).update(hash).digest('hex');
-    }
-    var s = format
-              .replace('{username}', username)
-              .replace('{salt}', salt)
-              .replace('{hash}', hash)
-              .replace('{iterations}', iterations);
-    console.log(s);
-  });
+  if (type == 1) {
+    /* generate 128 bit crypto random bytes
+     * save the byte buffer as a base64 encoded salt
+     * using the salt and password create a salted sha512 hash
+     * output the username, salt, and salted hash ':' delimited
+     */
+    crypto.randomBytes(+bytes, function(err, buf) {
+      if (err) throw err;
+      var salt = buf.toString('base64');
+      var hash = result.password;
+      for (var i = 0; i<=iterations; i++) {
+        hash = crypto.createHmac('sha512', salt).update(hash).digest('hex');
+      }
+      var s = format
+                .replace('{username}', username)
+                .replace('{salt}', salt)
+                .replace('{hash}', hash)
+                .replace('{iterations}', iterations);
+      console.log(s);
+    });
+  }else{
+    var salt = bcrypt.genSaltSync(+rounds);
+    var hash = bcrypt.hashSync(result.password, salt);
+    console.log('%s:%s', username, hash);
+  }
 });
